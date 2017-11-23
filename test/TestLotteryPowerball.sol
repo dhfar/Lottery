04ffdb71ad4e66be3fcb5ef5837e53fc00ce0a1a
Wenilch <33628735+Wenilch@users.noreply.github.com>
2017-11-23 16:43:09 +0300

    Разработка смартконтракта PowerBall 23.11.17
    Добавил функции получения инфо о билете, лотерее.
    Добавил флаг указывающий, что лотерея ещё не разыграна.
    Тесты функции покупки билета (допустимые, граничные, недопустимые значения)

--- test/TestLotteryPowerball.sol
+++ test/TestLotteryPowerball.sol
@@ -3,8 +3,17 @@ import "truffle/Assert.sol";
 import "../contracts/LotteryPowerball.sol";
 
 contract TestLotteryPowerball {
-    function BuyTickets() public {
+
+    function testBuyTickets() public {
         LotteryPowerball powerBall = new LotteryPowerball();
         Assert.isTrue(powerBall.buyTicket(1, 2, 3, 4, 5, 6), "Билет 1, 2, 3, 4, 5, 6 приобретён");
+        Assert.isTrue(powerBall.buyTicket(1, 2, 3, 4, 69, 26), "Билет 1, 2, 3, 4, 69, 26 приобретён");
+        Assert.isFalse(powerBall.buyTicket(1, 2, 3, 4, 5, 45), "Билет 1, 2, 3, 4, 5, 45 не приобретён");
+        Assert.isFalse(powerBall.buyTicket(1, 2, 3, 4, 69, 27), "Билет 1, 2, 3, 4, 69, 27 не приобретён");
+        Assert.isFalse(powerBall.buyTicket(1, 2, 3, 4, 70, 26), "Билет 1, 2, 3, 4, 70, 26 не приобретён");
+        Assert.isFalse(powerBall.buyTicket(1, 2, 3, 4, 70, 27), "Билет 1, 2, 3, 4, 70, 27 не приобретён");
+        Assert.isFalse(powerBall.buyTicket(0, 2, 3, 4, 5, 6), "Билет 0, 2, 3, 4, 5, 45 не приобретён");
+        Assert.isFalse(powerBall.buyTicket(0, 2, 3, 4, 5, 0), "Билет 0, 2, 3, 4, 5, 0 не приобретён");
+        Assert.isFalse(powerBall.buyTicket(1, 2, 3, 4, 5, 0), "Билет 1, 2, 3, 4, 5, 0 не приобретён");
     }
 }
