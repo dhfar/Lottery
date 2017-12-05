364cae90c3ae7a4bf30810243003e60a4f16f3b4
Wenilch <33628735+Wenilch@users.noreply.github.com>
2017-12-05 20:04:48 +0300

    Добавил функцию сравнения двух строк

--- contracts/RgbLottery.sol
+++ contracts/RgbLottery.sol
@@ -59,4 +59,26 @@ contract RgbLottery is DHFBaseCurrency {
         }
         return string(result);
     }
+
+    function compareString(string _a, string _b) private returns (int) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint minLength = a.length;
+        if (b.length < minLength) minLength = b.length;
+        for (uint i = 0; i < minLength; i ++)
+            if (a[i] < b[i])
+                return -1;
+            else if (a[i] > b[i])
+                return 1;
+        if (a.length < b.length)
+            return -1;
+        else if (a.length > b.length)
+            return 1;
+        else
+            return 0;
+    }
+
+    function equalString(string a, string b) public returns (bool) {
+        return compareString(a, b) == 0;
+    }
 }
