cadec7ffa3f11f2eb36c1b4abe4c0d69c4871f15
Wenilch <33628735+Wenilch@users.noreply.github.com>
2018-02-01 13:22:09 +0300

    Commint 01.02.18 - добавил web3j и все его прелести

---
+++ java/bin/org/dhfar/Owned.java
@@ -0,0 +1,75 @@
+package org.dhfar;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 3.2.0.
+ */
+public class Owned extends Contract {
+    private static final String BINARY = "6060604052341561000f57600080fd5b61016d8061001e6000396000f3006060604052600436106100565763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416638da5cb5b811461005b578063df32754b1461008a578063f2fde38b1461009f575b600080fd5b341561006657600080fd5b61006e6100be565b604051600160a060020a03909116815260200160405180910390f35b341561009557600080fd5b61009d6100cd565b005b34156100aa57600080fd5b61009d600160a060020a03600435166100f7565b600054600160a060020a031681565b6000805473ffffffffffffffffffffffffffffffffffffffff191633600160a060020a0316179055565b60005433600160a060020a0390811691161461011257600080fd5b6000805473ffffffffffffffffffffffffffffffffffffffff1916600160a060020a03929092169190911790555600a165627a7a7230582061e7bfa8ff88536045c8fa4c85fd603f606cae0bfae0382bbadf148770f7594f0029";
+
+    protected Owned(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected Owned(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public RemoteCall<String> owner() {
+        Function function = new Function("owner", 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteCall<TransactionReceipt> owned() {
+        Function function = new Function(
+                "owned", 
+                Arrays.<Type>asList(), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<TransactionReceipt> transferOwnership(String newOwner) {
+        Function function = new Function(
+                "transferOwnership", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(newOwner)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public static RemoteCall<Owned> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Owned.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static RemoteCall<Owned> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Owned.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static Owned load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Owned(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    public static Owned load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Owned(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+}
